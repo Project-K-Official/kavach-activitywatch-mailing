29e398da15041389855a355bc0276f5dadc56b6f
Kabir Dhruw <34811605+Kiinitix@users.noreply.github.com>
2022-08-30 20:11:24 +0530

    Update generate_conf.py

--- src/generate_conf.py
+++ src/generate_conf.py
@@ -2,6 +2,7 @@ import configparser
 
 config_file = configparser.ConfigParser()
 config_file.add_section("SMTPlogin")
+config_file.add_section("AppConfig")
 
 # Add settings to section
 # Here you need to modify the values of last parameter of set function
@@ -9,6 +10,8 @@ config_file.set("SMTPlogin", "sender_address", "----")
 config_file.set("SMTPlogin", "receiver_address", "----")
 config_file.set("SMTPlogin", "mailtrap_user", "----")
 config_file.set("SMTPlogin", "mailtrap_password", "----")
+
+config_file.set("AppConfig", "time_interval_for_mail", "-----")
 
 # Saving config file as configurations.ini
 with open(r"configurations.ini", 'w') as configfileObj:
